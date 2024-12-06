5827d063bc1a8f09e0ee82b360fbb1d89779b46c
PancakeTAS <pancake@mgnet.work>
2024-12-06 18:33:01 +0100

    refactor: minor adjustments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
+use std::ptr::null_mut;
+
 use esp_idf_hal::delay::FreeRtos;
+use esp_idf_sys::{esp, esp_vfs_dev_uart_use_driver, uart_driver_install};
 
 mod cli;
 mod packet;
 mod queue;
 
 fn main() {
+    // setup the peripherals
+    esp_idf_svc::sys::link_patches();
+
+    unsafe {
+        esp!(uart_driver_install(0, 512, 512, 10, null_mut(), 0)).unwrap();
+        esp_vfs_dev_uart_use_driver(0);
+    }
+
+    // setup the logger
+    esp_idf_svc::log::EspLogger::initialize_default();
     println!("meow :3 arf~");
 
     // create the transmitter queue and cli state
-    let mut queue = unsafe { queue::init() };
+    let mut queue = unsafe { queue::Queue::new() };
     let mut state = cli::State::new();
 
     // main loop
@@ -18,6 +31,7 @@ fn main() {
         queue.tick();
 
         // check for input
+        // (note: seems to be the only reliable way to read a single character from stdin afaik)
         let char = unsafe { libc::getchar() };
         if char == -1 {
             FreeRtos::delay_ms(1);
@@ -47,42 +61,42 @@ fn main() {
 fn longcat() {
     println!(
         r#"Your command is too looooooooooong
-               _
-     __       / |
-     \ "-..--'_4|_
-_._____     \ _  _(C "._'._
-((^     '"-._( O_ O "._` '. \
-`"'--._     \  y_     \   \|
- '-._  \_ _  __.=-.__,\_
-     `'-(" ,("___       \,_____
-         (_,("___     .-./     '
-         |   C'___    (5)
-         /    ``  '---'-'._```
-        |     ```    |`    '"-._
-        |    ````    \-.`
-        |    ````    |  "._ ``
-        /    ````    |     '-.__
-       |     ```     |
-       |     ```     |
-       |     ```     |
-       |     ```     /
-       |    ````    |
-       |    ```     |
-       |    ```     /
-       |    ```     |
-       /    ```     |
-      |     ```     |
-      |     ```     !
-      |     ```    / '-.___
-      |    ````    !_      ''-
-      /   `   `    | '--._____)
-      |     /|     !
-      !    / |     /
-      |    | |    /
-      |    | |   /
-      |    / |   |
-      /   /  |   |
-     /   /   |   |
-    (,,_]    (,_,)    mozz   "#
+                           _
+                 __       / |
+                 \ "-..--'_4|_
+      _._____     \ _  _(C "._'._
+     ((^     '"-._( O_ O "._` '. \
+      `"'--._     \  y_     \   \|
+             '-._  \_ _  __.=-.__,\_
+                 `'-(" ,("___       \,_____
+                     (_,("___     .-./     '
+                     |   C'___    (5)
+                     /    ``  '---'-'._```
+                    |     ```    |`    '"-._
+                    |    ````    \-.`
+                    |    ````    |  "._ ``
+                    /    ````    |     '-.__
+                   |     ```     |
+                   |     ```     |
+                   |     ```     |
+                   |     ```     /
+                   |    ````    |
+                   |    ```     |
+                   |    ```     /
+                   |    ```     |
+                   /    ```     |
+                  |     ```     |
+                  |     ```     !
+                  |     ```    / '-.___
+                  |    ````    !_      ''-
+                  /   `   `    | '--._____)
+                  |     /|     !
+                  !    / |     /
+                  |    | |    /
+                  |    | |   /
+                  |    / |   |
+                  /   /  |   |
+                 /   /   |   |
+                (,,_]    (,_,)    mozz"#
     );
 }
